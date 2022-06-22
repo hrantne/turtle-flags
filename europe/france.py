f57f9a64474ed8b337a2979ce2909e1b0af99eca
hrantne <101862126+hrantne@users.noreply.github.com>
2022-06-22 15:08:16 +0400

    Create france.py

---
+++ europe/france.py
@@ -0,0 +1,70 @@
+import turtle
+turtle.title("France Flag In Python (by Hrant)")
+s = turtle.getscreen()
+t = turtle.Turtle()
+t.pensize(10)
+t.penup()
+
+sp = input("Set speed to: ")
+t.speed(int(sp))
+
+t.fillcolor("blue")
+t.begin_fill()
+t.goto(-240, 160)
+t.pendown()
+t.fd(480)
+t.rt(90)
+t.fd(320)
+t.rt(90)
+t.fd(480)
+t.rt(90)
+t.fd(320)
+t.rt(90)
+
+print("OUTLINE DONE")
+
+t.end_fill()
+
+print("BLUE FILL DONE")
+
+t.fd(160)
+t.fillcolor("white")
+t.begin_fill()
+t.fd(160)
+t.rt(90)
+t.fd(320)
+t.rt(90)
+t.fd(160)
+t.rt(90)
+t.fd(320)
+t.rt(90)
+
+print("WHITE OUTLINE DONE")
+
+t.end_fill()
+
+print("WHITE FILL DONE")
+
+t.fd(160)
+t.fillcolor("red")
+t.begin_fill()
+t.fd(160)
+t.rt(90)
+t.fd(320)
+t.rt(90)
+t.fd(160)
+t.rt(90)
+t.fd(320)
+t.rt(90)
+
+print("RED OUTLINE DONE")
+
+t.end_fill()
+
+print("RED FILL DONE")
+print("END")
+
+turtle.done()
+
+
+
